495b4cf9b81fdc5647115d96d8b519a1db67c9c8
Unknown <grenblot@gmail.com>
2020-03-03 11:39:07 +0100

    extract features from dataset images and export them to JSON for training

--- main.py
+++ main.py
@@ -7,31 +7,50 @@ import glob
 
 import numpy as np
 from sklearn import linear_model
+import pandas as pd
+import json
 
 X = []
 Y = []
 
-for image_filename in glob.glob("dataset/*"):
-    image = io.imread(image_filename)
-    print(image_filename)
-    color = image_filename.split(".")[0].split("\\")[-1].split("-")[0]
-    image_downscaled = resize(image, (10,10,3))
+def datasetToJSON():
+    images = []
+    for image_filename in glob.glob("dataset/*"):
+        image = io.imread(image_filename)
+        print(image_filename)
+        color = image_filename.split(".")[0].split("\\")[-1].split("-")[0]
+        image_downscaled = resize(image, (10,10,3))
 
-    X.append(image_downscaled.flatten())
-    Y.append(color)
+        image_dict = {
+            'filename':image_filename,
+            'features':list(image_downscaled.flatten()),
+            'color': color
+        }
+        images.append(image_dict)
+
+    with open('dataset.json', 'w') as json_file:
+        json.dump(images , json_file)
+
 
     # cv2.imshow('img',image_downscaled)
     # cv2.waitKey(0)
 
+# datasetToJSON()
+
+df = pd.read_json('dataset.json')
+df['features'] = df['features']
+
+X = list(df['features'])
+Y = list(df['color'])
+print(df)
 # pick a random image from the dataset,
 # remove it from the training dataset and use it for resting
 import random
 testing_n = random.randint(0,len(X)-1)
 testing_image = X.pop(testing_n)
 testing_class = Y.pop(testing_n)
-print(testing_class)
 
 clf = linear_model.SGDClassifier()
 clf.fit(X,Y)
 
-print(clf.predict([testing_image]))
+print("Test:", testing_class, '-->', clf.predict([testing_image]))
