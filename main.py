f9b27591c5812f0127ffb8f7894187d116410156
glhr <grenblot@gmail.com>
2020-03-10 13:09:43 +0100

    larger dataset + handle grayscale & transparent images + prediction score

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from skimage import io
 from skimage.transform import resize
+from skimage.color import rgb2hsv, gray2rgb
 from sklearn.model_selection import train_test_split
 import numpy as np
 import cv2
 import glob
 
-from sklearn import linear_model
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 import json
 
@@ -17,7 +19,13 @@ def datasetToJSON():
     for image_filename in glob.glob("dataset/*"):
         image = io.imread(image_filename)
         print(image_filename)
-        color = image_filename.split(".")[0].split("\\")[-1].split("-")[0]
+        color = image_filename.split(".")[0].split("\\")[-1].split("/")[-1].split("-")[0]
+        print(image.shape)
+        if len(image.shape) < 3:
+            image = gray2rgb(image)
+        elif image.shape[-1] > 3:
+            image = image[:,:,:3]
+        # hsv_img = rgb2hsv(image)
         image_downscaled = resize(image, (10,10,3))
 
         image_dict = {
@@ -52,8 +60,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.1)
 
 # train
-clf = linear_model.SGDClassifier()
+clf = MultinomialNB()
 clf.fit(X_train,y_train)
 
 # test classifier on unknown image(s) from testing dataset
-print("Test:\t", y_test, '\n-->\t', clf.predict(X_test))
+predicted = clf.predict(X_test)
+print("Test:\t", y_test, '\n-->\t', predicted)
+
+# calculate ratio of correct predictions
+correct = np.zeros(len(X_test))
+for i in range(len(X_test)):
+    correct[i] = 1 if (y_test[i] == predicted[i]) else 0
+print("Ratio of correct predictions:", np.round(np.sum(correct)/len(X_test),2))
