52f5d5522567bdabe47e621448dae270af5e1bc5
glhr <grenblot@gmail.com>
2020-04-30 16:08:57 +0200

    plot computation time for different histogram bin sizes

--- dataset_plots/eval_plots.R
+++ dataset_plots/eval_plots.R
@@ -13,11 +13,32 @@ eval$histo_bins = as.factor(eval$histo_bins)
 data.accuracy <- melt(eval, id.vars=c("X","histo_bins","channels"), measure.vars = c("accuracy"))
 boxplot <- ggplot(data.accuracy,aes(histo_bins, value))
 boxplot + geom_boxplot(aes(fill=channels)) +
+  stat_summary(fun=median,
+               geom="line",
+               aes(group=channels, color=channels),
+               position = position_dodge(width = 0.75),
+               show.legend = FALSE)  + 
+  stat_summary(fun=median,
+               geom="point",
+               aes(group=channels, color=channels),
+               position = position_dodge(width = 0.75),
+               show.legend = FALSE)  + 
   labs(x="Number of histogram bins",
        y="Cross-Validation accuracy\nacross all classifiers",
-       fill="Color space\nfor generating\nhistogram") +
+       fill="Histogram\ncolour space") +
   scale_fill_discrete(breaks=c("hsv", "rgb", "ycbcr"),
-                      labels=c("HSV", "RGB", "YCbCr")) +
-  stat_summary(fun=mean, geom="line", aes(group="channels"))  + 
-    stat_summary(fun=mean, geom="point")
+                      labels=c("HSV", "RGB", "YCbCr"))
 
+eval <- subset(eval_results, (histo_eq == 'False'))
+data.time <- melt(eval, id.vars=c("X","histo_bins","classifier"), measure.vars = c("time"))
+lineplot <- ggplot(data.time, aes(histo_bins, value, color=classifier))
+lineplot +
+  # geom_point(size=2) +
+  stat_summary(fun=mean,
+               geom="point",
+               aes(group=classifier, color=classifier),
+               show.legend = TRUE) +
+  stat_summary(fun=mean,
+               geom="line",
+               aes(group=classifier, color=classifier),
+               show.legend = FALSE)
