c396011cc7a48ed970821f00efe5e157ffed08ae
glhr <grenblot@gmail.com>
2020-04-30 11:40:46 +0200

    store eval results in dataframe and write to CSV file

--- eval.py
+++ eval.py
@@ -2,8 +2,11 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import LeaveOneOut
 
 import sys
+import pandas as pd
 sys.path.append('.')
 
+from utils.file import get_filename_from_path
+from utils.timing import CodeTimer
 from classifierutils import logger, classifier_params, standardize_data, load_dataset, classifier_dict
 
 
@@ -12,15 +15,18 @@ def eval_loo(X, Y, classifier):
     clf = classifier_dict[classifier]()
     if classifier in classifier_params:
         clf.set_params(**classifier_params[classifier])
-        logger.debug(clf.get_params())
 
-    X = standardize_data(X, classifier)
-
+    X = standardize_data(X, classifier)  # standardize data, method differs per classifier
     loo = LeaveOneOut()   # Leave-One-Out cross-validator
     cv_generator = loo.split(X)  # generates indices to split data into training & test set
-    scores = cross_val_score(clf, X, Y, cv=cv_generator)  # list of accuracy score for each split
+    with CodeTimer() as timer:
+        scores = cross_val_score(clf, X, Y, cv=cv_generator)  # list of accuracy score for each split
+    time = timer.took
+    accuracy = scores.mean()
 
-    logger.info("-> Accuracy: {:0.2f}".format(scores.mean()))  # average score
+    logger.info("-> Accuracy: {:0.2f}, Time: {}".format(accuracy, time))  # average score and inference time
+
+    return accuracy, time, clf.get_params()
 
 
 if __name__ == '__main__':
@@ -33,10 +39,25 @@ if __name__ == '__main__':
     # ]
     # classifiers = list(classifier_dict.keys())
     classifiers = ['SGDClassifier','BernoulliNB','MultinomialNB']
+    rows = []
     import glob
     for dataset in glob.glob('src/color_classifier/dataset_json/*.json'):
-        logger.info(dataset)
+        filename = get_filename_from_path(dataset, extension=False)
+        logger.info(filename)
+        channels, histo_bins, eq = filename.split('-')[1:]
         X, Y = load_dataset(path=dataset)
-        logger.info("Dataset size: {}".format(len(X)))
+        # logger.info("Dataset size: {}".format(len(X)))
         for classifier in classifiers:
-            eval_loo(X, Y, classifier)
+            accuracy, time, params = eval_loo(X, Y, classifier)
+            rows.append({
+                'classifier': classifier,
+                'channels': channels,
+                'histo_bins':int(histo_bins),
+                'histo_eq': True if len(eq) else False,
+                'accuracy': accuracy,
+                'time': time
+            })
+
+    df = pd.DataFrame(rows)
+    logger.info(df)
+    df.to_csv('src/color_classifier/dataset_plots/eval_results.csv')
